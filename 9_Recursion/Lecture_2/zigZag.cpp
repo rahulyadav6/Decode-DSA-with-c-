cdb74db97b45fbce8d328a1049a2fd5f82c79ef6
Rahul Kumar Yadav <ry573870@gmail.com>
2024-09-06 09:55:28 +0530

    Lecture 2 complete

---
+++ 9_Recursion/Lecture_2/zigZag.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+void zigZag(int n){
+    if(n==0) return;
+    cout<<n;
+    zigZag(n-1);
+    cout<<n;
+    zigZag(n-1);
+    cout<<n;
+}
+int main(){
+    zigZag(3);
+}
