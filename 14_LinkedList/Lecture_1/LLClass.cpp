02a963444e3709a5c2ec4f173c8bffa7322735f9
Rahul Kumar Yadav <ry573870@gmail.com>
2024-10-14 18:09:43 +0530

    linked list part 1 complete

---
+++ 14_LinkedList/Lecture_1/LLClass.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+using namespace std;
+class Node{
+public:
+    int val;
+    Node* next;
+    Node(int val){
+        this->val = val;
+        this->next = NULL;
+    }
+};
+class LinkedList{
+public:
+    Node* head;
+    Node* tail;
+    int size;
+    LinkedList(){
+        head = tail = NULL;
+        size = 0;
+    }
+    void insertAtTail(int val){
+        Node* temp = new Node(val);
+        if(size == 0){  
+            head = tail = temp;
+        }else{
+            tail->next = temp;
+            tail = temp;
+        }
+        size++;
+    }
+    void insertAtHead(int val){
+        Node* temp = new Node(val);
+        if(size == 0) head = tail = temp;
+        else{
+            temp->next = head;
+            head = temp;
+        }
+        size++;
+    }
+    void insertAtIdx(int idx, int val){
+        if(idx<0 || idx >size) cout<<"Invalid index.";
+        else if(idx ==0) insertAtHead(val);
+        else if(idx == size) insertAtTail(val);
+        else{
+            Node* t = new Node(val);
+            Node* temp = head;
+            for(int i=1;i<=idx-1;i++){
+                temp = temp->next;
+            }
+            t->next = temp->next;
+            temp->next = t;
+            size++;
+        }
+    };
+    void display(){
+        Node* temp = head;
+        while(temp != NULL){
+            cout<<temp->val<<" ";
+            temp = temp->next;
+        }
+        cout<<endl;
+    }
+};
+int main(){
+    LinkedList ll;
+    ll.insertAtTail(10);
+    ll.display();
+    ll.insertAtTail(20);
+    ll.display();
+    ll.insertAtTail(30);
+    ll.insertAtTail(40);
+    ll.display();
+    ll.insertAtHead(50);
+    ll.display();
+    ll.insertAtHead(24);
+    ll.display();
+    ll.insertAtIdx(4,80);
+    ll.display();
+}
