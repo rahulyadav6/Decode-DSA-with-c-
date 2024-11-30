d571252f88df164ce1b3948bfc7f56655af8333f
Rahul Kumar Yadav <ry573870@gmail.com>
2024-11-30 17:53:37 +0530

    heaps lecture basics done

---
+++ 20_Heaps/Lecture_1/1_basicMaxHeap.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+int main(){
+    // priority_queue<int>pq;
+    // pq.push(10);
+    // pq.push(5);
+    // pq.push(20);
+    // pq.push(-6);
+
+    // cout<<pq.top();
+
+
+    priority_queue< int, vector<int>, greater<int> >pq; //min heap
+}
---
+++ 20_Heaps/Lecture_1/2_kthSmallest.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+int main(){
+    int arr[] = {10,20,-4,6,18,2,105,118};
+    int k = 3;
+    int n = sizeof(arr)/sizeof(arr[0]);
+    priority_queue<int>pq;
+    for(int i=0; i<n; i++){
+        pq.push(arr[i]);
+        if(pq.size() > k){
+            pq.pop();
+        }
+    }
+    
+
+    cout<<pq.top();
+
+
+}
