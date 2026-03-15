b85239a7ba6214399faa0efb7b61d99d18a9275f
Rahul Kumar Yadav <ry573870@gmail.com>
2026-03-15 13:50:29 +0530

    pattern printing 2 complete

--- 2_PatternPrinting/Lecture_1/starPlus.cpp
+++ 2_PatternPrinting/Lecture_1/starPlus.cpp
@@ -2,13 +2,15 @@
 using namespace std;
 int main(){
     int n;
-    cout<<"Enter n: ";
+    cout<<"Enter an odd number: ";
     cin>>n;
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n;j++){
-            if((i==(n/2+1)) || (j==(n/2+1))){
-                cout<<"* ";
-            }else cout<<"  ";
+    for(int i=1;i<=n; i++){
+        for(int j=1; j<=n; j++){
+            if(i==(n/2+1) || j== (n/2+1)){
+                cout<<"*";
+            }else{
+                cout<<" ";
+            }
         }
         cout<<endl;
     }
--- 2_PatternPrinting/Lecture_1/starTriangleFlipped.cpp
+++ 2_PatternPrinting/Lecture_1/starTriangleFlipped.cpp
@@ -1,15 +1,29 @@
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"Enter n: ";
+//     cin>>n;
+//     for(int i=1;i<=n;i++){
+//         for(int j=1;j<=n-i;j++){
+//             cout<<" ";
+//         }
+//         for(int k=1; k<=i; k++){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+// }
 #include <iostream>
 using namespace std;
 int main(){
     int n;
     cout<<"Enter n: ";
     cin>>n;
-    for(int i=1;i<=n;i++){
-        for(int j=1;j<=n-i;j++){
-            cout<<" ";
-        }
-        for(int k=1; k<=i; k++){
-            cout<<"*";
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=n; j++){
+            if(i+j>=n+1)cout<<"*";
+            else cout<<" ";
         }
         cout<<endl;
     }
