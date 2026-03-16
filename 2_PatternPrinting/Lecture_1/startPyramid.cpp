cd335b9eabb8b76d37a778ebe9f39fbad91e7c7b
Rahul Kumar Yadav <ry573870@gmail.com>
2026-03-16 10:47:11 +0530

    nst and nsp method

--- 2_PatternPrinting/Lecture_1/startPyramid.cpp
+++ 2_PatternPrinting/Lecture_1/startPyramid.cpp
@@ -1,17 +1,43 @@
+// #include <iostream>
+// using namespace std;
+// int main(){
+//     int n;
+//     cout<<"No of rows: ";
+//     cin>>n;
+//     for(int i=1; i<=n; i++){
+//         for(int j=1; j<=n-i; j++){
+//             cout<<" ";
+//         }
+//         for(int k=1; k<=i; k++){
+//             cout<<"*";
+//         }
+//         for(int l=1; l<i; l++){
+//             cout<<"*";
+//         }
+//         cout<<endl;
+//     }
+//     return 0;
+// }
+
+
+// nsp and nst method;
+
+
 #include <iostream>
 using namespace std;
 int main(){
     int n;
     cout<<"No of rows: ";
     cin>>n;
+    int nsp=0;
+    int nst=0;
     for(int i=1; i<=n; i++){
-        for(int j=1; j<=n-i; j++){
+        nsp=n-i;
+        nst=2*i-1;
+        for(int j=1; j<=nsp; j++){
             cout<<" ";
         }
-        for(int k=1; k<=i; k++){
-            cout<<"*";
-        }
-        for(int l=1; l<i; l++){
+        for(int k=1; k<=nst; k++){
             cout<<"*";
         }
         cout<<endl;
